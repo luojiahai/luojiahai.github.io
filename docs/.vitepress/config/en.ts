91aefaca56df1dab40cee43315aefe7fbda193f7
luojiahai <ljiahai@hotmail.com>
2025-03-18 00:10:51 +1100

    Update en.ts

--- docs/.vitepress/config/en.ts
+++ docs/.vitepress/config/en.ts
@@ -3,21 +3,21 @@ import { defineConfig, type DefaultTheme } from 'vitepress'
 export const en = defineConfig({
   lang: 'en-US',
   title: 'luojiahai',
-  description: 'This is a useless site.',
+  description: 'this is a useless site.',
 
   themeConfig: {
     nav: nav(),
 
     footer: {
-      copyright: `Copyright © 2019-${new Date().getFullYear()} luojiahai`,
+      copyright: `copyright © 2019-${new Date().getFullYear()} luojiahai`,
     },
   },
 });
 
 function nav(): DefaultTheme.NavItem[] {
   return [
-    { text: 'Home', link: '/' },
-    { text: 'About', link: '/about' },
-    { text: 'CV', link: '/cv' },
+    { text: 'home', link: '/' },
+    { text: 'about', link: '/about' },
+    { text: 'resume', link: '/cv' },
   ];
 }
