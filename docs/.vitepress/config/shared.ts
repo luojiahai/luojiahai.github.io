cc3d1d96a4777e7b414c2fae0aeae7498705e544
luojiahai <ljiahai@hotmail.com>
2025-08-22 00:49:28 +1000

    Update

--- docs/.vitepress/config/shared.ts
+++ docs/.vitepress/config/shared.ts
@@ -32,6 +32,16 @@ export const shared = defineConfig({
     ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }],
     ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Cascadia+Code:ital,wght@0,200..700;1,200..700&display=swap' }],
     ['link', { rel: 'stylesheet', href: 'https://use.typekit.net/htd7ieb.css' }],
+    ['script', {}, `
+      (function(d) {
+        var config = {
+          kitId: 'sdz6fkm',
+          scriptTimeout: 3000,
+          async: true
+        },
+        h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+      })(document);`
+    ],
     ['meta', { name: 'theme-color', content: '#FF6699' }],
     ['meta', { property: 'og:type', content: 'website' }],
     ['meta', { property: 'og:locale', content: 'en' }],
