87afcc139d6f7665b1168eec79721dfbcf9d9b76
luojiahai <ljiahai@hotmail.com>
2025-09-14 16:19:34 +1000

    Update

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,10 +1,4 @@
 import DefaultTheme from "vitepress/theme";
 import "./style.css";
-import Modal from "../../components/Modal.vue";
 
-export default {
-  extends: DefaultTheme,
-  enhanceApp({ app }) {
-    app.component("Modal", Modal);
-  },
-};
+export default DefaultTheme;
