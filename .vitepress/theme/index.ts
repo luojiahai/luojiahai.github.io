482c76dac713b552f1effdc3a5c3d374ce43a6fe
luojiahai <ljiahai@hotmail.com>
2025-09-19 08:49:20 +1000

    Update

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,6 +1,6 @@
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
-import "./style.css";
+import "./custom.css";
 import Badge from "../../components/Badge.vue";
 
 export default {
