38f1c9ac998c6f83faa979c641e5f1d6be49ef49
luojiahai <ljiahai@hotmail.com>
2026-04-06 16:27:42 +1000

    fix: use absolute path for apple touch icon

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
   head: [
     ['link', { rel: 'shortcut icon', href: '/favicon.ico' }],
     ['link', { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
-    ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' }],
+    ['link', { rel: 'apple-touch-icon', href: 'https://luojiahai.com/apple-touch-icon.png' }],
     ['link', { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png', sizes: '32x32' }],
     ['link', { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png', sizes: '16x16' }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
